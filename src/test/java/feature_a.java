8b6427fe1630aa30703beedce30df3d8a8e66349
Mayasa79 <mayasa.sabie@gmail.com>
2021-01-26 14:28:05 -0600

    agree

--- src/test/java/feature_a.java
+++ src/test/java/feature_a.java
@@ -2,7 +2,7 @@ public class feature_a {
     public static void main(String[] args) {
         System.out.println("Godd job guys");
         System.out.println("Finggally");
-        System.out.println("SDET");
+        System.out.println("ggfdnew ");
         System.out.println("Akbar");
         // this is my history
     }
